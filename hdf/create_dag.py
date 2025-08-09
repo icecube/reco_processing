3c5e697936498d8e46e7ef48a3aee54ca5bebbc7
Thijs van Eeden <tvaneede@cobalt-10.icecube.wisc.edu>
2025-08-09 04:40:41 -0500

    first try ftp hdf after removal

--- hdf/create_dag.py
+++ hdf/create_dag.py
@@ -9,7 +9,7 @@ sys.path.append("/data/user/tvaneede/GlobalFit/reco_processing")
 from datasets import datasets
 
 # set the inputs
-reco_version = "spice_tau_reco"
+reco_version = "v6"
 
 # Dynamically select the desired dataset
 simulation_datasets = getattr(datasets, reco_version)
