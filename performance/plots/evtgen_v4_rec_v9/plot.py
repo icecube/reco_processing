5cb986a36404c4c6c435b948817fa3b4b891474c
Thijs van Eeden <tvaneede@cobalt-10.icecube.wisc.edu>
2025-09-08 04:53:11 -0500

    add extra plots evtgenv 4 rec v9

--- performance/plots/evtgen_v4_rec_v9/plot.py
+++ performance/plots/evtgen_v4_rec_v9/plot.py
@@ -18,68 +18,6 @@ from plot_utils import *
 
 slc = np.s_[:]
 
-def compare_evtgen( nufiles, labels, outpath ):
-    print("compare")
-
-    os.system(f"mkdir -p {outpath}")
-
-    # get the data
-    data = []
-    for nufile in nufiles: data += [ extract_data_combined(nufile) ]
-
-    print("Datasets with", [len(dataset) for dataset in data])
-
-    nbins = 20
-
-    # length
-    plot_lenres_len( data, fit_keys = ["len_Taupede","len_EventGeneratorDC_Thijs","len_EventGeneratorDC_Max","len_EventGeneratorDC_Combined"], 
-                     true_key = "len_tru_tianlu",labels = labels, outpath=outpath, bins = np.linspace(0, 100,30) )
-    plot_lenres_len( data, fit_keys = ["len_Taupede","len_EventGeneratorDC_Thijs","len_EventGeneratorDC_Max","len_EventGeneratorDC_Combined"], 
-                     true_key = "len_tru_neha",labels = labels, outpath=outpath, bins = np.linspace(0, 100,30) )
-
-    plot_len_fit( data, fit_keys = ["len_Taupede","len_EventGeneratorDC_Thijs","len_EventGeneratorDC_Max","len_EventGeneratorDC_Combined"], 
-                  labels=labels, outpath=outpath, bins = np.linspace(0, 100,nbins ) )
-
-    # energy asymmetry
-    plot_medasmres_len( data, fit_keys = ["asm_Taupede","asm_EventGeneratorDC_Thijs","asm_EventGeneratorDC_Max","asm_EventGeneratorDC_Combined"], 
-                        asm_true_key = "asm_tru_tianlu", len_tru_key="len_tru_tianlu",labels=labels, outpath=outpath, bins = np.linspace(0, 100,nbins) )
-    plot_medasmres_len( data, fit_keys = ["asm_Taupede","asm_EventGeneratorDC_Thijs","asm_EventGeneratorDC_Max","asm_EventGeneratorDC_Combined"], 
-                        asm_true_key = "asm_tru_tianlu", len_tru_key="len_tru_neha",labels=labels, outpath=outpath, bins = np.linspace(0, 100,nbins) )
-
-    # energy
-    plot_mederes( data, fit_keys = ["loge_Taupede","loge_EventGeneratorDC_Thijs","loge_EventGeneratorDC_Max","loge_EventGeneratorDC_Combined"], 
-                  true_key = "loge_tru_tianlu",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
-    plot_mederes( data, fit_keys = ["loge_Taupede","loge_EventGeneratorDC_Thijs","loge_EventGeneratorDC_Max","loge_EventGeneratorDC_Combined"], 
-                  true_key = "loge_tru_neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
-
-    plot_mederes( data, fit_keys = ["loge_Monopod","loge_EventGeneratorDC_Thijs","loge_EventGeneratorDC_Max","loge_EventGeneratorDC_Combined"], 
-                  true_key = "loge_tru_tianlu",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
-    plot_mederes( data, fit_keys = ["loge_Monopod","loge_EventGeneratorDC_Thijs","loge_EventGeneratorDC_Max","loge_EventGeneratorDC_Combined"], 
-                  true_key = "loge_tru_neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
-
-    # direction
-    plot_medangres( data, fit_keys = ["Taupede","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
-                   true_key="tianlu",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
-    plot_medangres( data, fit_keys = ["Taupede","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
-                   true_key="neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
-
-    plot_medangres( data, fit_keys = ["Monopod","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
-                   true_key="tianlu",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
-    plot_medangres( data, fit_keys = ["Monopod","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
-                   true_key="neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
-
-    plot_medangres( data, fit_keys = ["SPEFit16","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
-                   true_key="tianlu",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
-    plot_medangres( data, fit_keys = ["SPEFit16","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
-                   true_key="neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
-
-    plot_medangres_len( data, fit_keys = ["Taupede","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
-                        true_key="tianlu",labels=labels, outpath=outpath, bins = np.linspace(0, 100,15) )
-    plot_medangres_len( data, fit_keys = ["Taupede","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
-                        true_key="neha",labels=labels, outpath=outpath, bins = np.linspace(0, 100,15) )
-
-
-
 def compare( nufiles, outpath ):
     print("compare")
 
@@ -156,6 +94,111 @@ def compare( nufiles, outpath ):
                   labels=["Monopod","Millipede","MillipedeTrun"], outpath=outpath, bins = np.linspace(4,8,nbins) )
 
 
+def compare_evtgen( nufiles, labels, outpath ):
+    print("compare")
+
+    os.system(f"mkdir -p {outpath}")
+
+    # get the data
+    data = []
+    for nufile in nufiles: data += [ extract_data_combined(nufile) ]
+
+    print("Datasets with", [len(dataset) for dataset in data])
+
+    nbins = 20
+
+    # length
+    plot_lenres_len( data, fit_keys = ["len_Taupede","len_EventGeneratorDC_Thijs","len_EventGeneratorDC_Max","len_EventGeneratorDC_Combined"], 
+                     true_key = "len_tru_tianlu",labels = labels, outpath=outpath, bins = np.linspace(0, 100,30) )
+    plot_lenres_len( data, fit_keys = ["len_Taupede","len_EventGeneratorDC_Thijs","len_EventGeneratorDC_Max","len_EventGeneratorDC_Combined"], 
+                     true_key = "len_tru_neha",labels = labels, outpath=outpath, bins = np.linspace(0, 100,30) )
+
+    plot_len_fit( data, fit_keys = ["len_Taupede","len_EventGeneratorDC_Thijs","len_EventGeneratorDC_Max","len_EventGeneratorDC_Combined"], 
+                  labels=labels, outpath=outpath, bins = np.linspace(0, 100,nbins ) )
+
+    # energy asymmetry
+    plot_medasmres_len( data, fit_keys = ["asm_Taupede","asm_EventGeneratorDC_Thijs","asm_EventGeneratorDC_Max","asm_EventGeneratorDC_Combined"], 
+                        asm_true_key = "asm_tru_tianlu", len_tru_key="len_tru_tianlu",labels=labels, outpath=outpath, bins = np.linspace(0, 100,nbins) )
+    plot_medasmres_len( data, fit_keys = ["asm_Taupede","asm_EventGeneratorDC_Thijs","asm_EventGeneratorDC_Max","asm_EventGeneratorDC_Combined"], 
+                        asm_true_key = "asm_tru_tianlu", len_tru_key="len_tru_neha",labels=labels, outpath=outpath, bins = np.linspace(0, 100,nbins) )
+
+    # energy
+    plot_mederes( data, fit_keys = ["loge_Taupede","loge_EventGeneratorDC_Thijs","loge_EventGeneratorDC_Max","loge_EventGeneratorDC_Combined"], 
+                  true_key = "loge_tru_tianlu",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
+    plot_mederes( data, fit_keys = ["loge_Taupede","loge_EventGeneratorDC_Thijs","loge_EventGeneratorDC_Max","loge_EventGeneratorDC_Combined"], 
+                  true_key = "loge_tru_neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
+
+    plot_mederes( data, fit_keys = ["loge_Monopod","loge_EventGeneratorDC_Thijs","loge_EventGeneratorDC_Max","loge_EventGeneratorDC_Combined"], 
+                  true_key = "loge_tru_tianlu",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
+    plot_mederes( data, fit_keys = ["loge_Monopod","loge_EventGeneratorDC_Thijs","loge_EventGeneratorDC_Max","loge_EventGeneratorDC_Combined"], 
+                  true_key = "loge_tru_neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
+
+    # direction
+    plot_medangres( data, fit_keys = ["Taupede","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
+                   true_key="tianlu",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
+    plot_medangres( data, fit_keys = ["Taupede","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
+                   true_key="neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
+
+    plot_medangres( data, fit_keys = ["Monopod","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
+                   true_key="tianlu",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
+    plot_medangres( data, fit_keys = ["Monopod","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
+                   true_key="neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
+
+    plot_medangres( data, fit_keys = ["SPEFit16","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
+                   true_key="tianlu",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
+    plot_medangres( data, fit_keys = ["SPEFit16","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
+                   true_key="neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins) )
+
+    plot_medangres_len( data, fit_keys = ["Taupede","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
+                        true_key="tianlu",labels=labels, outpath=outpath, bins = np.linspace(0, 100,15) )
+    plot_medangres_len( data, fit_keys = ["Taupede","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
+                        true_key="neha",labels=labels, outpath=outpath, bins = np.linspace(0, 100,15) )
+
+def compare_evtgen_clean( nufiles, labels, outpath ):
+    print("compare")
+
+    os.system(f"mkdir -p {outpath}")
+
+    # get the data
+    data = []
+    for nufile in nufiles: data += [ extract_data_combined(nufile) ]
+
+    print("Datasets with", [len(dataset) for dataset in data])
+
+    nbins = 20
+
+    # length
+    plot_lenres_len( data, fit_keys = ["len_Taupede","len_EventGeneratorDC_Thijs","len_EventGeneratorDC_Max","len_EventGeneratorDC_Combined"], 
+                     true_key = "len_tru_tianlu",labels = labels, outpath=outpath, bins = np.linspace(0, 100,30), plot_quartiles=False )
+
+    plot_len_fit( data, fit_keys = ["len_Taupede","len_EventGeneratorDC_Thijs","len_EventGeneratorDC_Max","len_EventGeneratorDC_Combined"], 
+                  labels=labels, outpath=outpath, bins = np.linspace(0, 100,nbins ) )
+
+    # # energy asymmetry
+    plot_medasmres_len( data, fit_keys = ["asm_Taupede","asm_EventGeneratorDC_Thijs","asm_EventGeneratorDC_Max","asm_EventGeneratorDC_Combined"], 
+                        asm_true_key = "asm_tru_tianlu", len_tru_key="len_tru_tianlu",labels=labels, outpath=outpath, bins = np.linspace(0, 100,nbins), plot_quartiles=False )
+
+    # energy
+    plot_mederes( data, fit_keys = ["loge_Taupede","loge_EventGeneratorDC_Thijs","loge_EventGeneratorDC_Max","loge_EventGeneratorDC_Combined"], 
+                  true_key = "loge_tru_neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins), plot_quartiles=True )
+
+    plot_mederes( data, fit_keys = ["loge_Monopod","loge_EventGeneratorDC_Thijs","loge_EventGeneratorDC_Max","loge_EventGeneratorDC_Combined"], 
+                  true_key = "loge_tru_neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins), plot_quartiles=True )
+
+    # direction
+    plot_medangres( data, fit_keys = ["Taupede","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
+                   true_key="neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins), plot_quartiles=False )
+
+    plot_medangres( data, fit_keys = ["Monopod","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
+                   true_key="neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins), plot_quartiles=False )
+
+    plot_medangres( data, fit_keys = ["SPEFit16","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
+                   true_key="neha",labels=labels, outpath=outpath, bins = np.linspace(4,8,nbins), plot_quartiles=False )
+
+    plot_medangres_len( data, fit_keys = ["Taupede","EventGeneratorDC_Thijs","EventGeneratorDC_Max","EventGeneratorDC_Combined"], 
+                        true_key="tianlu",labels=labels, outpath=outpath, bins = np.linspace(0, 100,15), plot_quartiles=False )
+
+
 ###
 ### NuTau
 ###
@@ -172,6 +215,14 @@ def compare( nufiles, outpath ):
 # outpath = "/data/user/tvaneede/GlobalFit/reco_processing/performance/plots/evtgen_v4_rec_v9/NuTau_EvtGen"
 # compare_evtgen(nufiles=nufiles, labels = labels,outpath=outpath)
 
+nufiles = ["/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuTau.h5",
+           "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuTau.h5",
+           "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuTau.h5",
+           "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuTau.h5"]
+labels = ["Taupede", "EvtGen Thijs", "EvtGen Max", "EvtGen Combined"]
+outpath = "/data/user/tvaneede/GlobalFit/reco_processing/performance/plots/evtgen_v4_rec_v9/NuTau_EvtGen_clean"
+compare_evtgen_clean(nufiles=nufiles, labels = labels,outpath=outpath)
+
 ###
 ### NuE
 ###
@@ -181,13 +232,13 @@ def compare( nufiles, outpath ):
 # compare(nufiles=nufiles,outpath=outpath)
 
 
-nufiles = ["/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuE.h5",
-           "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuE.h5",
-           "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuE.h5",
-           "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuE.h5"]
-labels = ["Taupede", "EvtGen Thijs", "EvtGen Max", "EvtGen Combined"]
-outpath = "/data/user/tvaneede/GlobalFit/reco_processing/performance/plots/evtgen_v4_rec_v9/NuE_EvtGen"
-compare_evtgen(nufiles=nufiles, labels = labels,outpath=outpath)
+# nufiles = ["/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuE.h5",
+#            "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuE.h5",
+#            "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuE.h5",
+#            "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuE.h5"]
+# labels = ["Taupede", "EvtGen Thijs", "EvtGen Max", "EvtGen Combined"]
+# outpath = "/data/user/tvaneede/GlobalFit/reco_processing/performance/plots/evtgen_v4_rec_v9/NuE_EvtGen"
+# compare_evtgen(nufiles=nufiles, labels = labels,outpath=outpath)
 
 # ###
 # ### NuMu
@@ -197,11 +248,11 @@ compare_evtgen(nufiles=nufiles, labels = labels,outpath=outpath)
 # outpath = "/data/user/tvaneede/GlobalFit/reco_processing/performance/plots/evtgen_v4_rec_v9/NuMu"
 # compare(nufiles=nufiles,outpath=outpath)
 
-nufiles = ["/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuMu.h5",
-           "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuMu.h5",
-           "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuMu.h5",
-           "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuMu.h5"]
-labels = ["Taupede", "EvtGen Thijs", "EvtGen Max", "EvtGen Combined"]
-outpath = "/data/user/tvaneede/GlobalFit/reco_processing/performance/plots/evtgen_v4_rec_v9/NuMu_EvtGen"
-compare_evtgen(nufiles=nufiles, labels = labels,outpath=outpath)
+# nufiles = ["/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuMu.h5",
+#            "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuMu.h5",
+#            "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuMu.h5",
+#            "/data/user/tvaneede/GlobalFit/reco_processing/hdf/output/evtgen_v4_rec_v9/NuMu.h5"]
+# labels = ["Taupede", "EvtGen Thijs", "EvtGen Max", "EvtGen Combined"]
+# outpath = "/data/user/tvaneede/GlobalFit/reco_processing/performance/plots/evtgen_v4_rec_v9/NuMu_EvtGen"
+# compare_evtgen(nufiles=nufiles, labels = labels,outpath=outpath)
 
