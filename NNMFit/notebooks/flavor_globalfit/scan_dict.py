a466ac87a9da9189769a9afe7177da053ea6fa80
Thijs van Eeden <tvaneede@cobalt-15.icecube.wisc.edu>
2026-02-19 04:45:05 -0600

    update notebooks nnmfit

---
+++ NNMFit/notebooks/flavor_globalfit/scan_dict.py
@@ -0,0 +1,67 @@
+
+
+dag_path       = "/data/user/tvaneede/GlobalFit/reco_processing/NNMFit/dag_scans/flavor_globalfit"
+hese_path      = f"{dag_path}/hese"
+globalfit_path = f"{dag_path}/globalfit"
+combined_path  = f"{dag_path}/globalfit_hese"
+
+scan_dir_dict = {
+    # --- benchmark ---
+    "spice_nosyst":           f"{hese_path}/asimov_SAY_HESEBestfit_NoSystematics_spice",
+    "hese_oldpid_nosyst":     f"{hese_path}/asimov_SAY_HESEBestfit_NoSystematics_ftp_FinalTopology",
+
+    # # --- Old PID with systematics ---
+    # "hese_oldpid":            f"{hese_path}/asimov_SAY_HESEBestfit_ftp_FinalTopology",
+
+    # # --- 11 feature variants, optimized for different bdt trainings or figures of merit ---
+    # "hese_11feat_purity":     f"{hese_path}/mcd-flavor_flux-hese_feat-11features/"
+    #                           f"bdt1_0.21_bdt2_0.55_length_10",
+
+    # "hese_11feat_fisher":     f"{hese_path}/mcd-flavor_flux-hese_feat-11features/"
+    #                           f"bdt1_0.366667_bdt2_0.433333_length_10",
+
+    # "hese_11feat_simple":     f"{hese_path}/mcd-simpletopology_flux-hese_feat-11features/"
+    #                           f"bdt1_0.366667_bdt2_0.433333_length_10",
+
+    # # --- All features (old configs, not yet optimized) ---
+    # "hese_allfeat_old":       f"{hese_path}/mcd-simpletopology_flux-hese_feat-11features_plus_"
+    #                           f"rloglmilli_econf_evtgen/bdt1_0.4_bdt2_0.45_length_10",
+
+    # "hese_allfeat_bdtprod_old": f"{hese_path}/mcd-simpletopology_flux-hese_feat-11features_plus_"
+    #                             f"rloglmilli_econf_evtgen/"
+    #                             f"bdt1_0.3_bdt2_0.4_length_10_10bdtprod_threshold_0.12",
+
+    # # --- trying smaller length teaser ---
+    # "hese_allfeat_len6":      f"{hese_path}/mcd-simpletopology_flux-hese_feat-11features_plus_"
+    #                           f"rloglmilli_econf_evtgen/bdt1_0.4_bdt2_0.45_length_6.31",
+
+    # "hese_allfeat_len8":      f"{hese_path}/mcd-simpletopology_flux-hese_feat-11features_plus_"
+    #                           f"rloglmilli_econf_evtgen/bdt1_0.4_bdt2_0.45_length_8",
+
+    # # --- All features (current configs) ---
+    # "hese_allfeat":           f"{hese_path}/mcd-simpletopology_flux-hese_feat-11features_plus_"
+    #                           f"rloglmilli_econf_evtgen/bdt1_0.366667_bdt2_0.5_length_10",
+
+    # "hese_allfeat_bdtprod":   f"{hese_path}/mcd-simpletopology_flux-hese_feat-11features_plus_"
+    #                           f"rloglmilli_econf_evtgen/"
+    #                           f"bdt1_0.333333_bdt2_0.366667_length_10_10bdtprod_threshold_0.122",
+
+    "hese_allfeat_bdtprod_combinedBase":
+                              f"{hese_path}/mcd-simpletopology_flux-hese_feat-11features_plus_"
+                              f"rloglmilli_econf_evtgen/"
+                              f"bdt1_0.333333_bdt2_0.366667_length_10_"
+                              f"10bdtprod_threshold_0.122_combinedBaseline",
+
+    # --- Global fits ---
+    "gf_zheyang":             f"{globalfit_path}/asimov_SAY_globalfit_zheyang",
+    "gf_v0":                  f"{globalfit_path}/asimov_SAY_globalfit_datasets_22612_22644_train_zheyang_bdt1_0.45_bdt2_0.67",
+    "gf_v0_noHESE":           f"{globalfit_path}/asimov_SAY_globalfit_datasets_22612_22644_train_zheyang_bdt1_0.45_bdt2_0.67_noHESE",
+
+    # --- Combined globalfit HESE
+    "gfh_fluxh_woverlap" :    f"{combined_path}/asimov_SAY_globalfit_hese_flux-hese",
+    "gfh_fluxg_woverlap" :    f"{combined_path}/asimov_SAY_globalfit_hese_flux-globalfit",
+
+    "gfh_fluxh" :             f"{combined_path}/asimov_SAY_globalfit_hese_flux-hese_22612_22644_train_zheyang_bdt1_0.45_bdt2_0.67_noHESE",
+    "gfh_fluxg" :             f"{combined_path}/asimov_SAY_globalfit_hese_flux-globalfit_22612_22644_train_zheyang_bdt1_0.45_bdt2_0.67_noHESE",
+
+}
---
+++ NNMFit/notebooks/plot_utils_triangle.py
@@ -0,0 +1,105 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.font_manager as font_manager
+
+rcParams = {"axes.titlesize": 16,"axes.labelsize": 14,"xtick.labelsize": 14,"ytick.labelsize": 14}
+mpl.rcParams.update(rcParams)
+font_axis_label = {'family': 'serif','color':  'black','weight': 'normal','size': 22}
+font_title = {'family': 'serif','color':  'black','weight': 'bold','size': 20}
+font_legend = font_manager.FontProperties(family='serif',weight='normal',style='normal', size=13)
+
+# triangle plotting settings
+from Ternary import flavor_triangle
+plt.rcParams["figure.figsize"] = (6,6)
+plt.rcParams.update({'font.family':'serif'})
+
+ts_dict = {
+    '68%' : 2.37,
+    '90%' : 4.605,
+    '95%' : 5.99,
+}
+
+colours = ["black","C0","C3","C2", "C1", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11"]
+linestyles = ["-","--",":"]
+
+def compare_contours( data, names, labels, levels = ['68%'], title = r"HESE: $\phi_0 = 2.12,\gamma=2.87$", savepath = None ):
+
+    # helper functions
+    C = {} # needed for area comparison!
+
+    fig = plt.figure()
+    tax = flavor_triangle()
+
+    lh, ll = [], []
+
+    asimov = tax.ca.scatter([1.0/3], [1.0/3], marker='*', facecolor='black',
+                    edgecolor='k', lw=0.5, s=80)
+    lh.append(asimov)
+    ll.append('Asimov: 1:1:1')
+
+    fmt={} # set labels to the contours
+
+    ts_values = [ts_dict[i] for i in levels]
+
+    for i,name in enumerate(names):
+        C[name] = tax.ca.contour(data[name]["ft_grid_asimov_poisson"],
+                                data[name]["fe_grid_asimov_poisson"],
+                                data[name]["ts_grid_asimov_poisson"], 
+                                ts_values,
+                                linestyles=linestyles[:len(levels)],
+                                linewidths=1.5,
+                                colors=colours[i])
+        
+        h, _ = C[name].legend_elements()
+        lh.append(h[0]) # legend elements
+        ll.append(labels[i])
+
+        # add ts levels in plot
+        if i == len(names) - 1:
+            for l, s in zip(C[name].levels, levels):
+                fmt[l] = s
+            plt.clabel(C[name],ts_values,inline=True,fontsize=12.,
+                    fmt=fmt,colors='black')
+
+    l = fig.legend(lh,ll,
+                    bbox_to_anchor=(0.7, 0.05),prop=font_legend,
+                    ncols=1 if len(names) < 5 else 2,frameon=True,fancybox=True,shadow=True)
+    if title: plt.title(title,y=1.1,fontdict=font_title)    
+
+    if savepath: plt.savefig(savepath,bbox_inches='tight')
+
+    return C
+
+def polygon_area(x, y):
+    # Close path if not already closed
+    if x[0] != x[-1] or y[0] != y[-1]:
+        x = np.append(x, x[0])
+        y = np.append(y, y[0])
+    return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
+
+def compare_area( C, alt_key, base_key, levels ):
+    areas = {}
+
+    ts_values = [ts_dict[i] for i in levels]
+
+    for name in [alt_key,base_key]: 
+        # print(f"Processing {name}")
+        contour_obj = C[name]
+        area_list = []
+        for i, level in enumerate(levels):  # 68%, 90%
+            paths = contour_obj.collections[i].get_paths()
+            level_area = 0.0
+            for p in paths:
+                v = p.vertices
+                x, y = v[:, 0], v[:, 1]
+                level_area += polygon_area(x, y)
+            area_list.append(level_area)
+            # print(f"Area for level {ts_values[i]} ({level}): {level_area:.4f}")
+        areas[name] = area_list
+
+    # Now calculate and print ratio of areas at 68% CL (index 0) and 90% CL (index 1)
+    for i, level in enumerate(levels):
+        ratio = areas[alt_key][i] / areas[base_key][i]
+        print(f"Area ratio ({alt_key} / {base_key}) at {level}: {ratio:.4f}")
